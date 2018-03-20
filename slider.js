a9eead4742c41fce2ad430c28146921c2510d28f
Rocky <postiglionedavide@gmail.com>
2018-03-20 16:28:01 +0100

    chi_siamo done !

--- slider.js
+++ slider.js
@@ -36,38 +36,4 @@ window.onload = function () {
      };
 
  };
-
-
-
- 	// toggle menu function
-     var menu = document.querySelector("#menu");
-
-     menu.onclick = function () {
- 
-         var list = document.querySelector(".sideNav");
-         var close = document.querySelector("#close")
-         var overlay = document.createElement("div");
-         overlay.setAttribute("id", "overlay");
- 
-         document.body.appendChild(overlay);
-         list.classList.add("open");
- 
-         overlay.onclick = function () {
-             list.classList.remove("open");
-             document.body.removeChild(overlay);
- 
-         }
- 
-         close.onclick = function () {
-             list.classList.remove("open");
-             document.body.removeChild(overlay);
-             
-         }
- 
- 
-     }
- 
-
-
-
 };
